63891e67edd6cc99ad70c7277a264ff53ba08a22
tk011 <tk011vip@gmail.com>
2017-03-17 10:00:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var infoContent = document.getElementById("infoContent");
+var testContent = document.getElementById("testContent");
+
+function show(value) {
+	value.style.display = "block";
+}
+
+function hide(value) {
+	value.style.display = "none";
+}
